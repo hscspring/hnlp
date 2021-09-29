3fe24524607658f44b388a9d645ba9eee0cabef4
hscspring <haoshaochun@gmail.com>
2021-09-29 23:02:17 +0800

    add layer config

--- hnlp/layer/pretrained_tf.py
+++ hnlp/layer/pretrained_tf.py
@@ -15,15 +15,22 @@ class PretrainedWord2vec(tfk.layers.Layer):
         trainable = not fix_pretrained
         pretrained = np.load(pretrained_path)
         embed_array = pretrained.astype("float32")
-        embed_tensor = tf.constant(embed_array)
-        vocab_size = embed_tensor.shape[0]
-        embed_size = embed_tensor.shape[1]
+        self.vocab_size = embed_array.shape[0]
+        self.embed_size = embed_array.shape[1]
         self.embed = tfk.layers.Embedding(
-            vocab_size,
-            embed_size,
-            embeddings_initializer=tf.keras.initializers.Constant(embed_tensor),
+            self.vocab_size,
+            self.embed_size,
+            embeddings_initializer=tfk.initializers.Constant(embed_array),
             trainable=trainable,
         )
+
+    def get_config(self):
+        config = super(PretrainedWord2vec, self).get_config()
+        config.update({
+            "vocab_size": self.vocab_size,
+            "embed_size": self.embed_size
+        })
+        return config
 
     def call(self, inputs):
         return self.embed(inputs)
@@ -41,10 +48,15 @@ class PretrainedBert(tfk.layers.Layer):
     def __init__(self, pretrained_path: str, fix_pretrained: bool):
         super(PretrainedBert, self).__init__()
         config_path = os.path.join(str(pretrained_path), "bert_config.json")
-        config = AutoConfig.from_pretrained(config_path)
+        self.config = AutoConfig.from_pretrained(config_path)
         trainable = not fix_pretrained
-        self.bert = TFAutoModel.from_config(config)
+        self.bert = TFAutoModel.from_config(self.config)
         self.bert.trainable = trainable
+
+    def get_config(self):
+        config = super(PretrainedBert, self).get_config()
+        config.update(self.config)
+        return config
 
     def call(self, inputs, mask, token_type_ids, training):
         return self.bert(
