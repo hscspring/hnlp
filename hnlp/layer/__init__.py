4e3e324ec26c40e303e8aad8b7e19cf0befbb4c7
hscspring <haoshaochun@gmail.com>
2021-11-09 10:52:52 +0800

    add attn

--- hnlp/layer/__init__.py
+++ hnlp/layer/__init__.py
@@ -3,5 +3,6 @@ from hnlp.config import ARCH
 
 if ARCH == "tf":
     from hnlp.layer.embeddings_tf import Embeddings
+    from hnlp.layer.attn_tf import InteractiveSelfAttention
 else:
     raise NotImplementedError
